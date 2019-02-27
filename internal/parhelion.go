9cd6c53c6f0c92656aa8a4c6bb2e9bfa01abd2ef
Rintaro Okamura <rintaro.okamura@gmail.com>
2019-02-28 00:19:03 +0900

    Add internal skeleton interface

--- internal/parhelion.go
+++ internal/parhelion.go
@@ -1,1 +1,54 @@
 package internal
+
+import (
+	parhelion "github.com/parheliondb/ParhelionDB"
+)
+
+type parhelionDBInternal struct{}
+
+type ParhelionDBInternal interface {
+	Get(key []byte, attemptNumber int) ([]byte, error)
+	Put(key []byte, value []byte) bool
+	Delete(key []byte) error
+	Close() error
+	Size() int64
+	SetIOErrorFlag() error
+	PauseCompaction() error
+	ResumeCompaction() error
+}
+
+func NewParhelionDBInternal(dirName string, options parhelion.ParhelionDBOptions) (ParhelionDBInternal, error) {
+	return &parhelionDBInternal{}, nil
+}
+
+func (p *parhelionDBInternal) Get(key []byte, attemptNumber int) ([]byte, error) {
+	return nil, nil
+}
+
+func (p *parhelionDBInternal) Put(key []byte, value []byte) bool {
+	return false
+}
+
+func (p *parhelionDBInternal) Delete(key []byte) error {
+	return nil
+}
+
+func (p *parhelionDBInternal) Close() error {
+	return nil
+}
+
+func (p *parhelionDBInternal) Size() int64 {
+	return 0
+}
+
+func (p *parhelionDBInternal) SetIOErrorFlag() error {
+	return nil
+}
+
+func (p *parhelionDBInternal) PauseCompaction() error {
+	return nil
+}
+
+func (p *parhelionDBInternal) ResumeCompaction() error {
+	return nil
+}
