c78d2aca75e8d11aa75dcb91821bcb8f2e897621
Rintaro Okamura <rintaro.okamura@gmail.com>
2019-03-01 19:03:17 +0900

    Add DBDirectory interface

--- db_directory.go
+++ db_directory.go
@@ -1,1 +1,51 @@
 package parhelion
+
+import "github.com/parheliondb/ParhelionDB/util"
+
+type dbDirectory struct {
+	Path string
+}
+
+type DBDirectory interface {
+	Close() error
+	GetPath() string
+	// ListDataFiles() []string
+	// ListIndexFiles() []int
+	// ListTombstoneFiles() []string
+	SyncMetaData() error
+}
+
+func NewDBDirectory(path string) (DBDirectory, error) {
+	err := util.CreateDirectoryIfNotExists(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dbDirectory{
+		Path: path,
+	}, nil
+}
+
+func (d *dbDirectory) Close() error {
+	return nil
+}
+
+func (d *dbDirectory) GetPath() string {
+	return d.Path
+}
+
+// func (d *dbDirectory) ListDataFiles() []string {
+//
+// }
+//
+// func (d *dbDirectory) ListIndexFiles() []int {
+//
+// }
+//
+// func (d *dbDirectory) ListTombstoneFiles() []string {
+//
+// }
+
+func (d *dbDirectory) SyncMetaData() error {
+	return nil
+}
--- util/file.go
+++ util/file.go
@@ -1,1 +1,18 @@
 package util
+
+import (
+	"fmt"
+	"os"
+)
+
+func CreateDirectoryIfNotExists(path string) error {
+	fi, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return os.Mkdir(path, 0660)
+	} else if !fi.Mode().IsDir() {
+		return fmt.Errorf("%s is not a directory", path)
+	}
+
+	return nil
+}
